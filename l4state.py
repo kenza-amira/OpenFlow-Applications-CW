914d64bcb5deb281480ffd6a84010ae23b3d59ea
kenza-amira <57840494+kenza-amira@users.noreply.github.com>
2022-04-18 15:11:13 +0100

    l4state first try

--- l4state.py
+++ l4state.py
@@ -42,6 +42,24 @@ class L4State14(app_manager.RyuApp):
         #
         # write your code here
         #
+        tcph = pkt.get_protocols(tcp.tcp)
+        iph = pkt.get_protocols(ipv4.ipv4)[0]
+        if len(tcph) > 0:
+            if in_port == 1:
+                acts = [psr.OFPActionOutput(2)]
+                self.ht.add((iph.src, iph.dst, in_port, 2))
+                match = psr.OFPMatch(in_port, eth.ethertype, iph.src, iph.dst, iph.proto, tcph.src_port, tcph.dst_port)
+                self.add_flow(dp, 1, match, msg.buffer_id)
+            else:
+                if (iph.dst, iph.src, 1, in_port) in self.ht:
+                    acts = [psr.OFPActionOutput(1)]
+                else:
+                    acts = [psr.OFPActionOutput(ofp.OFPPC_NO_FWD)]
+                match = psr.OFPMatch(in_port, eth.ethertype, iph.src, iph.dst, iph.proto, tcph.src_port, tcph.dst_port)
+                self.add_flow(dp, 1, match, msg.buffer_id)
+        else:
+            out_port = 1 if in_port == 2 else 2
+            acts = [psr.OFPActionOutput(out_port)]
         data = msg.data if msg.buffer_id == ofp.OFP_NO_BUFFER else None
         out = psr.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id,
                                in_port=in_port, actions=acts, data=data)
