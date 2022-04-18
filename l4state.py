5cd4614c5a702714bf6ede3ad83a938bc3197484
kenza-amira <57840494+kenza-amira@users.noreply.github.com>
2022-04-18 15:34:27 +0100

    try 4, specifying flags

--- l4state.py
+++ l4state.py
@@ -48,14 +48,18 @@ class L4State14(app_manager.RyuApp):
             if in_port == 1:
                 acts = [psr.OFPActionOutput(2)]
                 self.ht.add((iph.src, iph.dst, in_port, 2))
-                match = psr.OFPMatch(in_port, eth.ethertype, iph.src, iph.dst, iph.proto, tcph[0].src_port, tcph[0].dst_port)
+                match = psr.OFPMatch(in_port=in_port, eth_src=eth.src, eth_dst=eth.dst, 
+                                     ipv4_src=iph.src, ipv4_dst=iph.dst, tcp_src=tcph[0].src_port, 
+                                     tcp_dst=tcph[0].dst_port)
                 self.add_flow(dp, 1, match, acts, msg.buffer_id)
             else:
                 if (iph.dst, iph.src, 1, in_port) in self.ht:
                     acts = [psr.OFPActionOutput(1)]
                 else:
                     acts = [psr.OFPActionOutput(ofp.OFPPC_NO_FWD)]
-                match = psr.OFPMatch(in_port, eth.ethertype, iph.src, iph.dst, iph.proto, tcph[0].src_port, tcph[0].dst_port)
+                match = psr.OFPMatch(in_port=in_port, eth_src=eth.src, eth_dst=eth.dst, 
+                                     ipv4_src=iph.src, ipv4_dst=iph.dst, tcp_src=tcph[0].src_port, 
+                                     tcp_dst=tcph[0].dst_port)
                 self.add_flow(dp, 1, match, acts, msg.buffer_id)
         else:
             out_port = 1 if in_port == 2 else 2
