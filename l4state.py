2c8b05ba36f0ef9b5e3664adf88ffd6d6c9e548d
kenza-amira <57840494+kenza-amira@users.noreply.github.com>
2022-04-21 17:19:21 +0000

    correcting flow flags

--- l4state.py
+++ l4state.py
@@ -53,18 +53,19 @@ class L4State14(app_manager.RyuApp):
                                         ipv4_src=iph.src, ipv4_dst=iph.dst, tcp_src=tcph[0].src_port, 
                                         tcp_dst=tcph[0].dst_port)
                     self.add_flow(dp, 1, match, acts)
-            elif in_port == 2:
+            else:
                 if (iph.dst, iph.src, 1, in_port) in self.ht:
                     acts = [psr.OFPActionOutput(1)]
                 else:
                     acts = [psr.OFPActionOutput(ofp.OFPPC_NO_FWD)]
                 match = psr.OFPMatch(in_port=in_port, eth_src=eth.src, eth_dst=eth.dst, 
-                                     ipv4_src=iph.src, ipv4_dst=iph.dst, tcp_src=tcph[0].src_port, 
-                                     tcp_dst=tcph[0].dst_port)
+                                        ipv4_src=iph.src, ipv4_dst=iph.dst, tcp_src=tcph[0].src_port, 
+                                        tcp_dst=tcph[0].dst_port)
                 self.add_flow(dp, 1, match, acts)
         else:
             out_port = 1 if in_port == 2 else 2
             acts = [psr.OFPActionOutput(out_port)]
+            
         data = msg.data if msg.buffer_id == ofp.OFP_NO_BUFFER else None
         out = psr.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id,
                                in_port=in_port, actions=acts, data=data)
