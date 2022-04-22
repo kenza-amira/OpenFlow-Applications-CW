384c5ec24305b1998c53ad5a49b538024a4226b3
kenza-amira <57840494+kenza-amira@users.noreply.github.com>
2022-04-22 17:32:23 +0000

    invalid flags

--- l4state.py
+++ l4state.py
@@ -54,8 +54,8 @@ class L4State14(app_manager.RyuApp):
                                     ipv4_src=iph.src, ipv4_dst=iph.dst, tcp_src=tcph[0].src_port, 
                                     tcp_dst=tcph[0].dst_port)
                 self.add_flow(dp, 1, match, acts, msg.buffer_id)
-                if msg.buffer_id != ofp.OFP_NO_BUFFER:
-                    return
+                # if msg.buffer_id != ofp.OFP_NO_BUFFER:
+                #     return
             else:
                 if (iph.dst, iph.src, 1, in_port) in self.ht:
                     acts = [psr.OFPActionOutput(1)]
@@ -65,8 +65,8 @@ class L4State14(app_manager.RyuApp):
                                         ipv4_src=iph.src, ipv4_dst=iph.dst, tcp_src=tcph[0].src_port, 
                                         tcp_dst=tcph[0].dst_port)
                 self.add_flow(dp, 1, match, acts, msg.buffer_id)
-                if msg.buffer_id != ofp.OFP_NO_BUFFER:
-                    return
+                # if msg.buffer_id != ofp.OFP_NO_BUFFER:
+                #     return
         else:
             out_port = 1 if in_port == 2 else 2
             acts = [psr.OFPActionOutput(out_port)]
